2827ca27b3a1cb3ff0f76583c8ca7000b711dbb5
Felipe <cataofelipe@gmail.com>
2023-01-07 01:15:02 -0300

    update scene

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 import Oct8 from '../Oct8/Oct8.js'
 
-let element = new Oct8("square1",10,10,10,9,"sse sse-on","square1",true)
+let element = new Oct8("nave",90,40,10,10,"sse","square1",true)
+let Nave = document.getElementById("nave")
+element.ModifyProps(Nave,["red"],[element.PropsElement.backgroundColor,"red"])
+element.ModifyProps(Nave,10,[element.PropsElement.Rotate])
+//element.ModifyProps(Nave,"URL(../img/nave.png)",element.PropsElement.BackgroundImage)
